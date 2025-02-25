da408b98309996f4849d918ab261ced9a1d5caeb
griffdog82 <griffdog82x@gmail.com>
2025-02-25 17:01:07 -0500

    Refactor to use DBClass for database operations
    Refactored EditUser.cshtml.cs and ViewUsers.cshtml.cs to use the new DBClass for database operations, improving code maintainability and readability. Removed direct SqlConnection and SqlCommand usage, and moved related methods and models to DBClass. Updated views and page models accordingly. Added TODO and explanation comments for clarity.

--- Lab1_Part3_Johnson_Imlay/Pages/Admin/Users/EditUser.cshtml.cs
+++ Lab1_Part3_Johnson_Imlay/Pages/Admin/Users/EditUser.cshtml.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 using System.Collections.Generic;
 using Microsoft.VisualBasic;
+using Lab1_Part3_Johnson_Imlay.Pages.DB;
 
 namespace Lab1_Part3_Johnson_Imlay.Pages.Admin.Users
 {
     public class EditUserModel : PageModel
     {
-        private readonly string _connectionString = "Server=localhost;Database=Lab1;Trusted_Connection=True;";
-
+        // TODO: @Griffin -> I need to ensure that this is the mimimum amount of code needed to make this work.
         [BindProperty]
         public int UserID { get; set; }
 
@@ -40,49 +40,35 @@ namespace Lab1_Part3_Johnson_Imlay.Pages.Admin.Users
 
         public string Message { get; set; } = "";
 
-        public List<BusinessPartner> BusinessPartners { get; set; } = new();
+        public List<DBClass.BusinessPartner> BusinessPartners { get; set; } = new();
 
         public void OnGet(int id)
         {
             UserID = id;
-            LoadUser();
-            BusinessPartners = LoadBusinessPartners();
+            DBClass.LoadUsers();
+            BusinessPartners = DBClass.LoadBusinessPartners();
         }
 
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
             {
-                BusinessPartners = LoadBusinessPartners();
+                BusinessPartners = DBClass.LoadBusinessPartners();
                 return Page();
             }
 
             try
             {
-                using (SqlConnection conn = new SqlConnection(_connectionString))
+                bool success = DBClass.EditUser(UserID, Username, Email, FirstName, LastName, UserType, Department, AdminType, BusinessPartnerID);
+
+                if (success)
                 {
-                    conn.Open();
-                    string query = @"UPDATE [User] 
-                                    SET Username = @Username, Email = @Email, FirstName = @FirstName, LastName = @LastName,
-                                        UserType = @UserType, Department = @Department, AdminType = @AdminType, BusinessPartnerID = @BusinessPartnerID
-                                    WHERE UserID = @UserID";
-
-                    using (SqlCommand cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@UserID", UserID);
-                        cmd.Parameters.AddWithValue("@Username", Username);
-                        cmd.Parameters.AddWithValue("@Email", string.IsNullOrEmpty(Email) ? DBNull.Value : Email);
-                        cmd.Parameters.AddWithValue("@FirstName", FirstName);
-                        cmd.Parameters.AddWithValue("@LastName", LastName);
-                        cmd.Parameters.AddWithValue("@UserType", UserType);
-                        cmd.Parameters.AddWithValue("@Department", string.IsNullOrEmpty(Department) ? DBNull.Value : Department);
-                        cmd.Parameters.AddWithValue("@AdminType", string.IsNullOrEmpty(AdminType) ? DBNull.Value : AdminType);
-                        cmd.Parameters.AddWithValue("@BusinessPartnerID", BusinessPartnerID.HasValue ? BusinessPartnerID.Value : DBNull.Value);
-
-                        cmd.ExecuteNonQuery();
-                        Message = "User updated successfully!";
-                        return RedirectToPage("/Admin/Users/ViewUsers");
-                    }
+                    Message = "User updated successfully!";
+                    return RedirectToPage("/Admin/Users/ViewUsers");
+                }
+                else
+                {
+                    Message = "Error updating user.";
                 }
             }
             catch (Exception ex)
@@ -90,59 +76,14 @@ namespace Lab1_Part3_Johnson_Imlay.Pages.Admin.Users
                 Message = "Database Error: " + ex.Message;
             }
 
-            BusinessPartners = LoadBusinessPartners();
+            BusinessPartners = DBClass.LoadBusinessPartners();
             return Page();
-        }
+        } // TODO From Griffin -> Explanation for @Nicole/@Zach: This method will first check if the model state is valid. If it is not, it will reload the business partners and return the page. If it is valid, it will attempt to edit the user in the database. If the edit is successful, it will redirect to the ViewUsers page. If the edit is not successful, it will set the message to "Error updating user." and return the page. If there is an exception, it will set the message to "Database Error: " + the exception message and return the page.
 
-        private void LoadUser()
-        {
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                conn.Open();
-                using (SqlCommand cmd = new SqlCommand("SELECT Username, Email, FirstName, LastName, UserType, Department, AdminType, BusinessPartnerID FROM [User] WHERE UserID = @UserID", conn))
-                {
-                    cmd.Parameters.AddWithValue("@UserID", UserID);
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            Username = reader.GetString(0);
-                            Email = reader.IsDBNull(1) ? "N/A" : reader.GetString(1);
-                            FirstName = reader.GetString(2);
-                            LastName = reader.GetString(3);
-                            UserType = reader.GetString(4);
-                            Department = reader.IsDBNull(5) ? "N/A" : reader.GetString(5);
-                            AdminType = reader.IsDBNull(6) ? "N/A" : reader.GetString(6);
-                            BusinessPartnerID = reader.IsDBNull(7) ? null : reader.GetInt32(7);
-                        }
-                    }
-                }
-            }
-        }
 
-        private List<BusinessPartner> LoadBusinessPartners()
-        {
-            List<BusinessPartner> partners = new();
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                conn.Open();
-                using (SqlCommand cmd = new SqlCommand("SELECT BusinessPartnerID, Name FROM BusinessPartner", conn))
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        partners.Add(new BusinessPartner { BusinessPartnerID = reader.GetInt32(0), Name = reader.GetString(1) });
-                    }
-                }
-            }
-            return partners;
-        }
 
-        public class BusinessPartner
-        {
-            public int BusinessPartnerID { get; set; }
-            public string Name { get; set; } = "";
-        }
+
+
         public IActionResult OnPostPopulateHandler()
         {
             ModelState.Clear();
