9413229d49585f1bf71376f328719a07d81985ac
imlaygf <imlaygf@dukes.jmu.edu>
2025-02-24 21:18:59 -0500

    This includes new edits.

---
+++ FacultyDashboardModel.cs
@@ -0,0 +1,10 @@
+    namespace Lab1_Part3_Johnson_Imlay.Pages.Faculty
+    {
+        public class FacultyDashboardModel : PageModel
+        {
+            public void OnGet()
+            {
+            }
+        }
+    }
+    
